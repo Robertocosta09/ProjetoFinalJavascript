388450e6e404454a1e81b6571cb13bd47002b423
Roberto Costa <robertohsantos19@gmail.com>
2023-08-22 16:19:42 +0100

    Primeiraentrega

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-// Definição dos objetos de produtos
-const produtos = [
-    { id: 1, nome: 'Smartphone', preco: 1000 },
-    { id: 2, nome: 'Notebook', preco: 1500 },
-    { id: 3, nome: 'Fone de Ouvido', preco: 100 },
-    { id: 4, nome: 'Tablet', preco: 600 },
-];
+document.addEventListener('DOMContentLoaded', function () {
+    const addButtons = document.querySelectorAll('.add-btn');
+    const cartList = document.querySelector('.cart-list');
+    const cartTotal = document.querySelector('.cart-total');
+    const cartCount = document.querySelector('.cart-count');
+    let totalValue = 0;
 
-// Carrinho de compras
-const carrinho = [];
+    addButtons.forEach(addButton => {
+        addButton.addEventListener('click', function () {
+            const product = addButton.closest('.product');
+            const productName = product.dataset.name;
+            const productPrice = parseFloat(product.dataset.price);
 
-// Função para adicionar produto ao carrinho
-function adicionarAoCarrinho(id) {
-    const produto = produtos.find(item => item.id === id);
-    if (produto) {
-        carrinho.push(produto);
-        console.log(`Produto "${produto.nome}" adicionado ao carrinho.`);
-    } else {
-        console.log('Produto não encontrado.');
-    }
-}
+            const cartItem = document.createElement('li');
+            cartItem.textContent = `${productName} - R$${productPrice.toFixed(2)}`;
 
-// Função para listar produtos no carrinho
-function listarCarrinho() {
-    if (carrinho.length === 0) {
-        console.log('O carrinho está vazio.');
-    } else {
-        console.log('Produtos no carrinho:');
-        carrinho.forEach((produto, index) => {
-            console.log(`${index + 1}. ${produto.nome} - R$ ${produto.preco}`);
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remover';
+            removeButton.classList.add('remove-btn');
+
+            cartItem.appendChild(removeButton);
+            cartList.appendChild(cartItem);
+
+            totalValue += productPrice;
+            updateCartTotal();
+            updateCartCount();
+
+            removeButton.addEventListener('click', function () {
+                cartList.removeChild(cartItem);
+                totalValue -= productPrice;
+                updateCartTotal();
+                updateCartCount();
+            });
         });
+    });
+
+    function updateCartTotal() {
+        cartTotal.textContent = totalValue.toFixed(2);
     }
-}
 
-// Adicionar produtos ao carrinho
-adicionarAoCarrinho(1);
-adicionarAoCarrinho(3);
+    function updateCartCount() {
+        const cartItemCount = cartList.children.length;
+        cartCount.textContent = cartItemCount;
+    }
+});
 
-// Listar produtos no carrinho
-listarCarrinho();
